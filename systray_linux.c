a8d9a9b9fdb5df8e85457f952733da572184d972
Florian Brinker <mail+gitlab@f-brinker.de>
2020-11-16 23:11:51 +0100

    test

--- systray_linux.c
+++ systray_linux.c
@@ -13,6 +13,7 @@ static char temp_file_name[PATH_MAX] = "";
 typedef struct {
 	GtkWidget *menu_item;
 	int menu_id;
+	long signalHandlerId;
 } MenuItemNode;
 
 typedef struct {
@@ -23,7 +24,6 @@ typedef struct {
 	short disabled;
 	short checked;
 	short isCheckable;
-	long signalHandlerId;
 } MenuItemInfo;
 
 void registerSystray(void) {
@@ -107,9 +107,9 @@ gboolean do_add_or_update_menu_item(gpointer data) {
 			gtk_menu_item_set_label(GTK_MENU_ITEM(item->menu_item), mii->title);
 
 			if (mii->isCheckable) {
-				g_signal_handler_block(GTK_CHECK_MENU_ITEM(item->menu_item), mii->signalHandlerId);
+				g_signal_handler_block(GTK_CHECK_MENU_ITEM(item->menu_item), item->signalHandlerId);
 				gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(item->menu_item), mii->checked == 1);
-				g_signal_handler_unblock(GTK_CHECK_MENU_ITEM(item->menu_item), mii->signalHandlerId);
+				g_signal_handler_unblock(GTK_CHECK_MENU_ITEM(item->menu_item), item->signalHandlerId);
 			}
 			break;
 		}
@@ -126,7 +126,13 @@ gboolean do_add_or_update_menu_item(gpointer data) {
 		}
 		int *id = malloc(sizeof(int));
 		*id = mii->menu_id;
-		mii->signalHandlerId = g_signal_connect_swapped(G_OBJECT(menu_item), "activate", G_CALLBACK(_systray_menu_item_selected), id);
+
+		long signalHandlerId = g_signal_connect_swapped(
+			G_OBJECT(menu_item),
+			"activate",
+			G_CALLBACK(_systray_menu_item_selected),
+			id
+		);
 
 		if (mii->parent_menu_id == 0) {
 			gtk_menu_shell_append(GTK_MENU_SHELL(global_tray_menu), menu_item);
@@ -144,6 +150,7 @@ gboolean do_add_or_update_menu_item(gpointer data) {
 
 		MenuItemNode* new_item = malloc(sizeof(MenuItemNode));
 		new_item->menu_id = mii->menu_id;
+		new_item->signalHandlerId = signalHandlerId;
 		new_item->menu_item = menu_item;
 		GList* new_node = malloc(sizeof(GList));
 		new_node->data = new_item;
